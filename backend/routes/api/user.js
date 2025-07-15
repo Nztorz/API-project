c6228443670c93a305bdcc7f8d1a4300112826e7
Nztorz <nztor@proton.me>
2025-07-15 08:58:43 -0600

    Add user ValidateSignup middleware

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -4,7 +4,30 @@ const bcrypt = require("bcryptjs");
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
-router.post("/", async (req, res, next) => {
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateSignup = [
+    check("email")
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage("Please provide a valid email."),
+    check("username")
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage("Please provide an username with at least 4 characters."),
+    check("username")
+        .not()
+        .isEmail()
+        .withMessage("Username cannot be an email"),
+    check("password")
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage("Password must be 6 chracters or more."),
+    handleValidationErrors
+];
+
+router.post("/", validateSignup, async (req, res, next) => {
     const { email, password, username } = req.body;
 
     // seed the model
